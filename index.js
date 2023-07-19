4ae9598b2b52a874e2a8e470ecbf55d036bfc36a
Clay Ellis <clayellis@me.com>
2023-07-18 19:37:46 -0600

    Use hardcoded dist/main.sh for now

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import child_process from 'child_process';
-import path from 'path';
-import url from 'url';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const exe = path.resolve(__dirname, 'main.sh');
+const exe = 'dist/main.sh';
+
 try {
     child_process.execFileSync(exe, {stdio: 'inherit'});
 } catch (e) {
