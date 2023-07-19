628905b54dfc0019c21f9a729d138584a0d49b88
Clay Ellis <clayellis@me.com>
2023-07-19 00:00:26 -0600

    Recompile index.js

--- dist/index.js
+++ dist/index.js
@@ -30,11 +30,12 @@ const external_child_process_namespaceObject = require("child_process");
 
 
 //const exe = '${GITHUB_WORKSPACE}/dist/main.sh';
+const workspace = process.env.GITHUB_WORKSPACE;
 
 try {
     console.log(__filename);
     console.log(__dirname);
-    console.log(`workspace: ${GITHUB_WORKSPACE}`);
+    console.log(workspace);
 
 //    console.log(`spawning ${exe}`);
 //    child_process.execFileSync(exe, {stdio: 'inherit'});
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import child_process from 'child_process';
 
 //const exe = '${GITHUB_WORKSPACE}/dist/main.sh';
+const workspace = process.env.GITHUB_WORKSPACE;
 
 try {
     console.log(__filename);
     console.log(__dirname);
-    console.log(`workspace: ${GITHUB_WORKSPACE}`);
+    console.log(workspace);
 
 //    console.log(`spawning ${exe}`);
 //    child_process.execFileSync(exe, {stdio: 'inherit'});
